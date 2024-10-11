e92c4106f359b0548b2df93ac67d4b60198a088d
MarMarcz <s24566@pjwstk.edu.pl>
2024-10-11 09:39:16 +0200

    feat: add faker and generate data

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const app = express();
+import express from 'express';
+import { faker } from '@faker-js/faker';
+import { generateCompany } from './assets/generate.js';
+
+const app = new express();
 const port = 3000;
 
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/:id', (req, res) => {
+  res.send(generateCompany(Number(req.params.id)));
 });
 
 app.listen(port, () => {
